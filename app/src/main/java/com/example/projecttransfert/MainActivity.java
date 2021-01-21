dde09b6285d3c2d1652f02771786599a96177e2e
Judith Lisette SAGNA <sagnajudith@gmail.com>
2021-01-21 16:21:48 +0000

    first commit

---
+++ app/src/main/java/com/example/projecttransfert/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.projecttransfert;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.ActionBar;
+import android.content.ClipData;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+private LinearLayout myLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        this.myLayout =  (LinearLayout) findViewById(R.id.myDynamicLayout);
+
+        ImageView image = new ImageView(this);
+        ViewGroup.LayoutParams params = new ActionBar.LayoutParams(100, 100);
+        image.setLayoutParams(params);
+        image.setBackgroundResource(R.drawable.logo);
+        myLayout.addView(image);
+    }
+@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item:
+                Toast.makeText(this, "Item selected" , Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.accueil:
+                Toast.makeText(this, "Accueil selected" , Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.envoi:
+                //Toast.makeText(this, "Envoi selected" , Toast.LENGTH_LONG).show();
+                Intent intent= new Intent(MainActivity.this,judithActivity.class);
+                startActivity(intent);
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
